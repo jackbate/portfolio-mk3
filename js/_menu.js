cbe192a5115d784d331d071cf7a4b05de045279a
jackbate <75363110+jackbate@users.noreply.github.com>
2021-07-01 11:26:21 +0100

    Flip cards for projects begin

---
+++ js/_menu.js
@@ -0,0 +1,9 @@
+function buttonclick(){
+    var menuList = document.getElementById("menu");
+    if (menuList.className == "menuOff"){
+        menuList.className = "menuOn";
+    }
+    else{
+        menuList.className = "menuOff";
+    }
+}
