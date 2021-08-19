7e812977572ad50cd757f1f8939ce6df44c00002
jackbate <75363110+jackbate@users.noreply.github.com>
2021-08-19 16:27:59 +0100

    new changes addition of code snippet page and otehr fixes

--- js/_menu.js
+++ js/_menu.js
@@ -34,15 +34,14 @@ $(document).ready(function() {
   });
 
 
-/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
-function openNav() {
-  document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
+// This function  allows the program to recognise the class 'menu-display' and change the value accordingly
+function toggleSlideMenu(x){
+ if(x.classList.contains('menu-display')){
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+ }
+ else{
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+ }
 }
-
-/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
-} 
-
